c7e55ba2cd5d27809180e2985ef618f04f177e04
GeorgeCloud <net.george.sky@gmail.com>
2022-03-27 22:03:04 -0700

    mockup sample data for frontend

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+var events = [{id: 0, title: 'street racing', description: 'full description is here.', owner_username: "kiirb", created_on: '12/08/1998'}]
+
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set("views", "./views");
 
 
-app.get('/',
-  (req, res) => {
-    res.render('home', { message: 'Wrong Page' })
-  }
-)
+app.get('/', (req, res) => {
+  res.render('home', { events: events })
+})
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}}!`)
