3463dad2cd0cfdb752bcef09c40653c4ae61b566
GeorgeCloud <net.george.sky@gmail.com>
2022-03-28 22:29:18 -0700

    cleanup UI

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-var events = [{id: 0, title: 'street racing', description: 'full description is here.', owner_username: "kiirb", created_on: '12/08/1998'}]
+var events = [
+  {id: 0, title: 'street racing', description: 'full description is here.', owner_username: 'kiirb', imgUrl: 'https://i.giphy.com/media/X8M3OBHBLRWDWVaLYd/giphy.webp', created_on: '12/08/1998'},
+  {id: 0, title: 'street racing', description: 'full description is here.', owner_username: 'kiirb', imgUrl: 'https://i.giphy.com/media/X8M3OBHBLRWDWVaLYd/giphy.webp', created_on: '12/08/1998'}
+]
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
