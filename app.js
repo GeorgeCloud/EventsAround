821a3273086db871b124424bbf15e532fa466726
GeorgeCloud <net.george.sky@gmail.com>
2022-04-04 16:48:49 -0700

    remove unused nav items

--- app.js
+++ app.js
@@ -17,63 +17,6 @@ app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', handlebars: allo
 app.set('view engine', 'handlebars');
 app.set("views", "./views");
 
-// app.get('/', (req, res) => {
-//   models.Event.findByPk(id=4).then(event => {
-//     event.destroy();
-//   })
-//   res.send('1')
-// })
-
-app.get('/events/new', (req, res) => {
-    res.render('events_new')
-})
-
-// Create
-app.post('/events', (req, res) => {
-  models.Event.create(req.body).then(event => {
-    res.redirect(`/events/${event.id}`);
-  }).catch((err) => {
-    console.log(err);
-  });
-})
-
-app.get('/events/:id', (req, res) => {
-  event_id = req.params.id
-  models.Event.findByPk(id=event_id).then(event => {
-    res.render('events_show', {event: event});
-  })
-});
-
-// UPDATE
-app.put('/events/:id', (req, res) => {
-  models.Event.findByPk(req.params.id).then(event => {
-    event.update(req.body).then(event => {
-      res.redirect(`/events/${req.params.id}`);
-    }).catch((err) => {
-      console.log(err);
-    });
-  }).catch((err) => {
-    console.log(err);
-  });
-});
-
-app.delete('/events/:id', (req, res) => {
-  event_id = req.params.id;
-  models.Event.findByPk(id=event_id).then(event => {
-    event.destroy();
-    res.redirect('/');
-  }).catch((err) => {
-    console.log(err);
-  });
-});
-
-app.get('/events/:id/edit', (req, res) => {
-  event_id = req.params.id
-  models.Event.findByPk(id=event_id).then(event => {
-    res.render('events_edit', {event: event})
-  })
-});
-
 app.listen(port, () => {
   console.log(`App listening on port ${port}}!`)
 })
--- controllers/events.js
+++ controllers/events.js
@@ -5,4 +5,55 @@ module.exports = function (app, models) {
       res.render('index', { events: events });
     });
   });
+
+  app.get('/events/new', (req, res) => {
+      res.render('events_new')
+  })
+
+  // Create
+  app.post('/events', (req, res) => {
+    models.Event.create(req.body).then(event => {
+      res.redirect(`/events/${event.id}`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  })
+
+  // Event Detail
+  app.get('/events/:id', (req, res) => {
+    event_id = req.params.id
+    models.Event.findByPk(id=event_id).then(event => {
+      res.render('events_show', {event: event});
+    })
+  });
+
+  // UPDATE
+  app.put('/events/:id', (req, res) => {
+    models.Event.findByPk(req.params.id).then(event => {
+      event.update(req.body).then(event => {
+        res.redirect(`/events/${req.params.id}`);
+      }).catch((err) => {
+        console.log(err);
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
+  });
+
+  app.delete('/events/:id', (req, res) => {
+    event_id = req.params.id;
+    models.Event.findByPk(id=event_id).then(event => {
+      event.destroy();
+      res.redirect('/');
+    }).catch((err) => {
+      console.log(err);
+    });
+  });
+
+  app.get('/events/:id/edit', (req, res) => {
+    event_id = req.params.id
+    models.Event.findByPk(id=event_id).then(event => {
+      res.render('events_edit', {event: event})
+    })
+  });
 }
