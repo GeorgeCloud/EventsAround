243e0ded198c5682a445cd64a47c929f7bc3cbd4
GeorgeCloud <net.george.sky@gmail.com>
2022-04-05 09:14:31 -0700

    send json

--- controllers/events.js
+++ controllers/events.js
@@ -2,7 +2,7 @@ module.exports = function (app, models) {
 
   app.get('/', (req, res) => {
     models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
-      res.render('index', { events: events });
+      res.send(events);
     });
   });
 
